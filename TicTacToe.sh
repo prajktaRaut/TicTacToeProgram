718fe65f89e7d7c5b3308ff5bd0398e1efc2dd0b
prajktaRaut <prajktasraut16@gmail.com>
2019-11-17 11:14:13 +0530

    Start the tic-tac-toe game with resetting board

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -2,4 +2,20 @@
 
 echo " Welcome to Tic-Tac-Toe game "
 
+#Constatnt
+MAXPOSITION=9
+
+#Array
+declare -a boardstructure
+
+
+function boardPattern()
+{
+for (( i=1; i<= MAXPOSITION; i++ ))
+do
+	boardstructure[$i]=0
+done 
+}
+
+boardPattern
 
